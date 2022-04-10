8060aedc7be94a744f204d30b0c507a2ee66fcad
Strevino2 <strevino2@hotmail.com>
2022-04-10 12:24:14 -0500

    functions working

--- main.js
+++ main.js
@@ -15,11 +15,22 @@ const displayDate = () => {
  
 
 // Write a JavaScript program to convert a number to a string.
+const convertNumberToString = (str) => {
+  console.log(typeof str)
+  const result = Number(str)
+  console.log(typeof result)
+  return result
+}
 
-
+console.log(convertNumberToString)
 
 // Write a JavaScript program to convert a string to the number.
-
+const convertStringToNumber = (num) => {
+  console.log(typeof num)
+  const result = String(num)
+  return result
+  console.log(typeof result)
+}
 
 
 // Write a JavaScript program that takes in different datatypes and prints out whether they are a:
@@ -29,22 +40,50 @@ const displayDate = () => {
   // * Number
   // * NaN
   // * String
-  
+
+  function checkDataType (element) {
+    return typeof element
+  }
+  console.log(checkDataType(5))
 
   
 // Write a JavaScript program that adds 2 numbers together.
+const sumOfTwoNumbers = (num1, num2) => {
+  return num1 + num2;
+}
 
-
-
+sumOfTwoNumbers(1,2)
+console.log(sumOfTwoNumbers)
 // Write a JavaScript program that runs only when 2 things are true.
+const bothAreTrue = (a,b) => {
+  if(a && b){
+  return true
+}else {
+  return false
+}
+}
 
 
-
+bothAreTrue(1,2)
 // Write a JavaScript program that runs when 1 of 2 things are true.
-
+const onlyOneTrue = (a,b) => {
+  if(a || b){
+  return true
+}else {
+  return false
+}
+}
 
 
 // Write a JavaScript program that runs when both things are not true.  
+const noneAreTrue = (a,b) => {
+  if(!a && !b){
+  return true
+}else {
+  return false
+}
+}
+
 
 // ***************************
 //         PART TWO
@@ -58,8 +97,9 @@ const displayDate = () => {
 // 6. go to `index.html` 
 // 7. create inputs, buttons and event listeners that render the code blocks you built above to the DOM.
 
-
-
+const numInput = document.getElementById("num2str")
+num2str.value
+console.log (num2str)
 
 // Additional Resources
 // Video1: https://player.vimeo.com/video/377147232
